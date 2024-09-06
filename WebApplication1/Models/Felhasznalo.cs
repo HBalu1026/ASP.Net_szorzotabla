b9bbf45fd5d5d167159fb7edb4aea96099079d2b
hernadi.balazs <hernadi.balazs@BURG-1E06-20>
2024-09-06 10:10:31 +0200

    Felhasználó

---
+++ WebApplication1/Models/Felhasznalo.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Felhasznalo
+    {
+        public string Nev { get; set; }
+        public string Email { get; set; }
+        public int SzulEv { get; set; }
+    }
+}
--- WebApplication1/Pages/Felhasznalo.cshtml.cs
+++ WebApplication1/Pages/Felhasznalo.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
 
 namespace WebApplication1.Pages
 {
     public class FelhasznaloModel : PageModel
     {
+        [BindProperty]
+        public Felhasznalo ujFelhasznalo {  get; set; } = new Felhasznalo();
+
+        public List<Felhasznalo> felhasznalok { get; set; } = new List<Felhasznalo>();
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            felhasznalok.Add(ujFelhasznalo);
+            return Page();
         }
     }
 }
